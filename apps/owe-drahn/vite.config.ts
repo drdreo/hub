3f1f0686f96175543a831df59cabfed4a0ce9406
DrDreo <code@drdreo.com>
2025-03-02 16:18:48 +0100

    migrate owe-drahn to nx structure

---
+++ apps/owe-drahn/vite.config.ts
@@ -0,0 +1,55 @@
+/// <reference types='vitest' />
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default defineConfig({
+    root: __dirname,
+    cacheDir: "../../node_modules/.vite/apps/owe-drahn",
+    server: {
+        port: 4200,
+        host: "localhost",
+    },
+    preview: {
+        port: 4300,
+        host: "localhost",
+    },
+    plugins: [
+        react(),
+        ViteImageOptimizer({
+            png: { quality: 80 },
+            jpeg: { quality: 75 },
+            webp: { quality: 80 },
+            avif: { quality: 70 },
+        }),
+        sentryVitePlugin({
+            org: "drdreo",
+            project: "owe-drahn",
+        }),
+    ],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    build: {
+        outDir: "./dist",
+        emptyOutDir: true,
+        reportCompressedSize: true,
+        sourcemap: true,
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+    },
+    test: {
+        watch: false,
+        globals: true,
+        environment: "jsdom",
+        include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        reporters: ["default"],
+        coverage: {
+            reportsDirectory: "./test-output/vitest/coverage",
+            provider: "v8",
+        },
+    },
+});
