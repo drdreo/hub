94942523abe638539204a5fd5c03c296e0cd7b72
DrDreo <code@drdreo.com>
2025-03-05 20:40:10 +0100

    update configs

---
+++ libs/tell-it/shared/utils/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    root: __dirname,
+    cacheDir: "../../../../node_modules/.vite/libs/tell-it/shared/utils",
+    plugins: [],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    test: {
+        watch: false,
+        globals: true,
+        environment: "node",
+        include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        reporters: ["default"],
+        coverage: { reportsDirectory: "./test-output/vitest/coverage", provider: "v8" },
+        reportsDirectory: "./test-output/vitest/coverage",
+        provider: "v8",
+        globals: true,
+        environment: "node",
+        include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        reporters: ["default"],
+        coverage: { reportsDirectory: "./test-output/vitest/coverage", provider: "v8" }
+    }
+});
