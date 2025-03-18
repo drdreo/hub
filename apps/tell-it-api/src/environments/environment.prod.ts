7f4dbf3dbd518c83ff9fe8aaf6ae85ed126bbefe
DrDreo <code@drdreo.com>
2025-03-18 20:28:09 +0100

    fix tellit API postgres integration

--- apps/tell-it-api/src/environments/environment.prod.ts
+++ apps/tell-it-api/src/environments/environment.prod.ts
@@ -1,23 +1,17 @@
 import type { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { parse as parseConnectionString } from "pg-connection-string";
 
 export function getProdConfig() {
     const dbUrl = process.env.DATABASE_URL;
     if (!dbUrl) {
         throw new Error("DATABASE_URL not set");
     }
-    const connectionOptions = parseConnectionString(dbUrl);
 
     return {
         production: true,
         allowList: ["https://tell-it.pages.dev", "https://tell-it.drdreo.com"],
         typeOrm: {
             type: "postgres",
-            host: connectionOptions.host,
-            port: connectionOptions.port,
-            user: connectionOptions.user,
-            password: connectionOptions.password,
-            database: connectionOptions.database,
+            url: dbUrl,
             autoLoadEntities: true,
             ssl: { rejectUnauthorized: false }
         } as TypeOrmModuleOptions
