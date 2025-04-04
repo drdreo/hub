3776c717542b2dad03e8a19f60351c1faaa85819
DrDreo <andreas.hahn@bannerflow.com>
2025-04-04 10:40:26 +0200

    reset

--- apps/csharp-api/program.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-
-var app = WebApplication.Create();
-app.MapGet("/", () => "Hello, Tech Demo 10:35!");
-app.Urls.Add("http://*:8080");
-app.Run();
