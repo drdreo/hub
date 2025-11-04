aa3b473fd1db753c8c34efa099048e229ade7435
DrDreo <code@drdreo.com>
2025-11-04 18:32:06 +0100

    use sqlite and fix some stuff

--- apps/tell-it-api/src/main.ts
+++ apps/tell-it-api/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
     const allowList = configService.get("allowList");
-    console.log(allowList);
+
     app.enableCors({
         origin: (origin: string, callback: (err: Error | null, origin?: any) => void) => {
             console.log({ origin });
